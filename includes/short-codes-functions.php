543b73a8c0eb50c542a303982de6185bf63cd676
mtpamir <mtpamir@yahoo.com>
2016-08-20 15:05:32 +0600

    initial release

--- includes/short-codes-functions.php
+++ includes/short-codes-functions.php
@@ -1,55 +1,83 @@
 <?php
 
-function test_shortcode_function($atts) {
-    $a = shortcode_atts( array(
-        'fake' => 'fake',
-        'real' => 'real',
-    ), $atts );
-
-    //return "foo = {$a['foo']}";
-
-    $return_string= "{$a['real']} is {$a['fake']}";
-    return $return_string;
-}
-
-function register_test_shortcode(){
-    add_shortcode('ehsan', 'test_shortcode_function');
-}
-
-add_action( 'init', 'register_test_shortcode');
-
 function wp_rpt_shortcode($atts)
 {
     $a = shortcode_atts(array(
-        'ids' => '1,2,3',
+        'ids' => '',
         'featured' => '',
+        'column' => '3'
     ), $atts);
 
-    //$return_string= "{$a['ids']} is {$a['featured']}";
+    if (!empty($a['ids'])):
+        $post_id_array = explode(",", $a['ids']);
+        $featured = $a['featured'];
+        $column_width = ($a['column'] == 3) ? 4 : (($a['column'] == 4) ? 3 : 4);
 
-    $post_id_array = explode(",", $a['ids']);
-    print_r($post_id_array);
-    $query = new WP_Query(array('post_type' => 'wp-rpt-cpt', 'post__in' => $post_id_array));
-// The Loop
-    $return_string = "";
-    ob_start();
-    if ($query->have_posts()) {
-        while ($query->have_posts()) {
+        $query = new WP_Query(array('post_type' => 'wp-rpt-cpt', 'post__in' => $post_id_array,'orderby' => 'ID', 'order'   => 'ASC'));
+        // The Loop
 
-            $query->the_post();
-            ?>
-            <h1><?php the_title() ?></h1>
-            <div class='post-content'><?php the_content() ?></div>
+        ob_start();
+        if ($query->have_posts()): ?>
+            <section class="pricing1">
+                <div class="wp-rpt-container-fluid">
+                    <div class="wp-rpt-row">
+
+                        <?php
+                        while ($query->have_posts()):
+
+                            $query->the_post();
+                            ?>
+                            <div class="wp-rpt-col-md-<?php echo $column_width ?>">
+                                <div class="column-inner">
+                                    <div
+                                        class="pricing-wrapper <?php echo (get_the_ID() == $featured) ? "featured" : "" ?>">
+                                        <div class="top">
+                                            <div class="price">
+                                            <span
+                                                class="price-currency"><?php echo get_post_meta(get_the_ID(), 'additional_fields_currency', true); ?></span>
+                                            <span
+                                                class="price-amount"><?php echo get_post_meta(get_the_ID(), 'additional_fields_price', true); ?></span>
+                                            <span
+                                                class="price-extra"><?php echo get_post_meta(get_the_ID(), 'additional_fields_duration', true); ?></span>
+                                            </div>
+                                            <div
+                                                class="price-title"><?php echo get_post_meta(get_the_ID(), 'additional_fields_additional-text', true); ?></div>
+                                            <div class="price-plan">
+                                            <span class="t1"
+                                                  style="border-color:#31aae2 #31aae2 #31aae2 transparent;"></span>
+                                                <?php the_title() ?>
+                                                <span class="t2"
+                                                      style="border-color:#31aae2 transparent #31aae2 #31aae2;"></span>
+                                            </div>
+                                        </div>
+                                        <div class="pricing-content">
+                                            <?php the_content() ?>
+                                            <div class="pricing-action-button">
+                                                <a class="btn btn-block"
+                                                   href="<?php echo get_post_meta(get_the_ID(), 'additional_fields_button-url', true); ?>"><?php echo get_post_meta(get_the_ID(), 'additional_fields_button-text', true); ?></a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <?php
+                        endwhile;
+                        ?>
+                    </div>
+            </section>
             <?php
-        }
-    }
-    else{
-    ?>
+        else:
+            ?>
+            Oops, there are no posts.
+            </div>
+            <?php
+        endif;
+    else:
+        ?>
+        Oops, there are no pricing table.
+        <?php
+    endif;
 
-    Oops, there are no posts.
-
-    <?php
-}
 
 // Reset Query
     wp_reset_query();
@@ -57,8 +85,9 @@ function wp_rpt_shortcode($atts)
     return ob_get_clean();
 }
 
-function register_rpt_shortcode(){
+function register_rpt_shortcode()
+{
     add_shortcode('wp_rpt', 'wp_rpt_shortcode');
 }
 
-add_action( 'init', 'register_rpt_shortcode');
+add_action('init', 'register_rpt_shortcode');
--- wp-responsive-pricing-table.php
+++ wp-responsive-pricing-table.php
@@ -13,40 +13,46 @@
  * Domain Path:  /languages/
  */
 
-defined( 'ABSPATH' ) or die( 'Keep Quit' );
+defined('ABSPATH') or die('Keep Quit');
 
-if(!class_exists('WP_Responsive_Pricing_Table')):
-    Class WP_Responsive_Pricing_Table{
-        public function __construct() {
+if (!class_exists('WP_Responsive_Pricing_Table')):
+    Class WP_Responsive_Pricing_Table
+    {
+        public function __construct()
+        {
             $this->constants();
             $this->includes();
-            //$this->hooks();
-
-            do_action( 'wp-responsive-pricing-table', $this );
+            $this->hooks();
+            do_action('wp-responsive-pricing-table', $this);
         }
 
-        public function constants() {
-            define( 'WP_RPT_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
-            define( 'WP_RPT_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
-            define( 'WP_RPT_PLUGIN_DIRNAME', dirname( plugin_basename( __FILE__ ) ) );
-            define( 'WP_RPT_PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
-            define( 'WP_RPT_PLUGIN_FILE', __FILE__ );
+        public function constants()
+        {
+            define('WP_RPT_PLUGIN_URL', plugin_dir_url(__FILE__));
+            define('WP_RPT_PLUGIN_DIR', plugin_dir_path(__FILE__));
+            define('WP_RPT_PLUGIN_DIRNAME', dirname(plugin_basename(__FILE__)));
+            define('WP_RPT_PLUGIN_BASENAME', plugin_basename(__FILE__));
+            define('WP_RPT_PLUGIN_FILE', __FILE__);
         }
 
-        public function includes() {
-            //include_once( 'includes/template-functions.php' );
-            include_once( 'includes/class-wp-rpt-cpt.php' );
-            include_once( 'includes/class-wp-rpt-meta-box.php' );
-            include_once( 'includes/short-codes-functions.php' );
+        public function includes()
+        {
+            include_once('includes/class-wp-rpt-cpt.php');
+            include_once('includes/class-wp-rpt-meta-box.php');
+            include_once('includes/short-codes-functions.php');
 
         }
-/*
-        public function hooks() {
-            add_action( 'after_setup_theme', array( $this, 'setup_environment' ) );
-            add_action( 'init', array( $this, 'init' ), 0 );
-            add_action( 'widgets_init', array( $this, 'register_widgets' ) );
+
+        public function hooks()
+        {
+            add_action('wp_enqueue_scripts', array($this, 'wp_rpt_scripts'));
         }
-*/
+
+        function wp_rpt_scripts()
+        {
+            wp_enqueue_style('wp_rpt_grid', plugins_url('/assets/css/grid.css', __FILE__), array(), '20160816');
+            wp_enqueue_style('wp_rpt_style', plugins_url('/assets/css/style.css', __FILE__), array(), '20160816');
+        }
     }
 
     new WP_Responsive_Pricing_Table();
